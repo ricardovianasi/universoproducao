62385ab1569af25a71598de4a89dbb885c6492b9
Ricardo Viana <ricardovianasi@gmail.com>
2016-09-06 12:28:43 -0300

    correcoes

---
+++ config/autoload/routes.testing.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'router' => array(
+        'routes' => array(
+            'arquivoemcartaz' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route'    => '/arquivoemcartaz',
+                )
+            )
+        )
+    )
+];
