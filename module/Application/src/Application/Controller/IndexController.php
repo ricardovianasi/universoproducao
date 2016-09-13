4603d0a9aa000dfb792e3b3120d3b2cc54cc7f5d
Ricardo Viana <ricardovianasi@gmail.com>
2016-09-13 19:11:50 -0300

    correcao

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,12 +1,20 @@
 <?php
 namespace Application\Controller;
 
+use Application\Entity\Banner\Banner;
+use Zend\View\Model\ViewModel;
+
 class IndexController extends SiteController
 {
     const SITE_ID = 1;
 
     public function indexAction()
     {
-        return [];
+        //banner
+        $bannerImages = $this->getRepository(Banner::class)->findBy(['site' => self::SITE_ID], ['order'=>'ASC']);
+
+        return new ViewModel([
+            'bannerImages' => $bannerImages,
+        ]);
     }
 }
