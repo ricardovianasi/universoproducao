521437ad8ec8e644a6936840abe4cc7191dc825c
Ricardo Viana <ricardovianasi@gmail.com>
2017-05-15 19:19:58 -0300

    correcao

--- config/autoload/routes.testing.php
+++ config/autoload/routes.testing.php
@@ -2,19 +2,17 @@
 return [
     'router' => array(
         'routes' => array(
-            'cineop2017' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route'    => '/cineop2017',
-                ),
-                'priority' => 1000000
-            ),
             'universoproducao' => array(
                 'type' => 'Literal',
                 'options' => array(
                     'route'    => '/',
-                ),
-                'priority' => 100000
+                )
+            ),
+            'cineop2017' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route'    => '/cineop2017',
+                )
             ),
         )
     )
