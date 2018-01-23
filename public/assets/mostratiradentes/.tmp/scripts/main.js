ca4d04c68603ae75a900b8f232784e3f14cbf8b5
Ricardo Viana <ricardovianasi@gmail.com>
2018-01-23 08:38:33 -0200

    correceos

--- public/assets/mostratiradentes/.tmp/scripts/main.js
+++ public/assets/mostratiradentes/.tmp/scripts/main.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
         items: 1,
         nav: false,
         navText: ["<button class='gallery__arrow gallery__arrow--left'><span class='icon icon-arrow-left'></span></button>", "<button class='gallery__arrow gallery__arrow--right'><span class='icon icon-arrow-right'></span></button>"],
-        dots: false,
+        dots: true,
         mouseDrag: false,
         lazyLoad: false,
         loop: false,
