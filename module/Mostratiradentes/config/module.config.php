930e2afa1e8ff5ad337d8e8c3638e289aabd2a6f
Ricardo Viana <ricardovianasi@gmail.com>
2018-02-19 08:43:44 -0300

    correceos

--- module/Mostratiradentes/config/module.config.php
+++ module/Mostratiradentes/config/module.config.php
@@ -30,7 +30,7 @@ return array(
                             ),
                             'defaults' => array(
                                 'controller' => Controller\PostController::class,
-                                'action' => 'index',
+                                'action' => 'index-sp',
                             ),
                         ),
                     ),
@@ -40,7 +40,7 @@ return array(
                             'route' => '/',
                             'defaults' => array(
                                 'controller'    => Controller\IndexController::class,
-                                'action'        => 'index',
+                                'action'        => 'index-sp',
                             ),
                         )
                     ),
