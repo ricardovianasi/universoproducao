b463cc03a47c26203591e1152f39db7ad9bf03d6
Ricardo Viana <ricardovianasi@gmail.com>
2018-04-02 13:20:42 -0300

    asdf

--- module/Cineop2018/config/module.config.php
+++ module/Cineop2018/config/module.config.php
@@ -153,13 +153,13 @@ return array(
     ),
     'view_manager' => array(
         'template_map' => array(
-            'cineop2017/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'cineop2018/layout' => __DIR__ . '/../view/layout/layout.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
     ),
     'module_layouts' => [
-        __NAMESPACE__ => 'cineop2017/layout'
+        __NAMESPACE__ => 'cineop2018/layout'
     ]
 );
