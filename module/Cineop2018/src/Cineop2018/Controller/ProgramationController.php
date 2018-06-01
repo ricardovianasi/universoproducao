ddbc0b1b33dd276e48d2f8f3e1de359e6d1784a1
Ricardo Viana <ricardovianasi@gmail.com>
2018-06-01 20:02:35 -0300

    correcoes

--- module/Cineop2018/src/Cineop2018/Controller/ProgramationController.php
+++ module/Cineop2018/src/Cineop2018/Controller/ProgramationController.php
@@ -114,7 +114,7 @@ class ProgramationController extends SiteController
                 if ($prog->getType() == Type::SESSION && $prog->hasMeta(Meta::SESSION_TITLE)) {
                     $titleItem .= self::SEPARATOR . $prog->hasMeta(Meta::SESSION_TITLE)->getValue();
                     foreach ($prog->getChildren() as $sess) {
-                        if($sess) {
+                        if($sess->getObject()) {
                             $events[] = [
                                 'title' => $sess->getObject()->getTitle()." | <span class=\"programing-direction\">Direção: ".$sess->getObject()->getDirection()."</span>",
                                 'id' => $sess->getObject()->getId()
