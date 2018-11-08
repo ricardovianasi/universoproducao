e8ef389b484cafd656b798f7f53158de9fbfb2c5
Ricardo Viana <ricardovianasi@gmail.com>
2018-11-08 09:45:20 -0200

    asdf

--- module/Application/src/Application/Repository/Proposal/ArtisticProposal.php
+++ module/Application/src/Application/Repository/Proposal/ArtisticProposal.php
@@ -12,5 +12,47 @@ use Util\Repository\AbstractRepository;
 
 class ArtisticProposal extends AbstractRepository
 {
+    public function prepareSearch($criteria = [], $orderBy = [])
+    {
+        $qb = $this->createQueryBuilder('p');
+
+        if(!empty($criteria['artist_name'])) {
+            $qb
+                ->andWhere('p.artistName like :artist_name')
+                ->setParameter('artist_name', '%'.$criteria['artist_name'].'%');
+        }
+
+
+        if(!empty($criteria['show_name'])) {
+            $qb
+                ->andWhere('p.showName like :show_name')
+                ->setParameter('show_name', '%'.$criteria['show_name'].'%');
+        }
+
+        if(!empty($criteria['category'])) {
+            $qb
+                ->andWhere('p.category = :category')
+                ->setParameter('category', '%'.$criteria['category'].'%');
+        }
+
+
+        if(!empty($criteria['startDate'])) {
+            $startDate = \DateTime::createFromFormat('d/m/Y', $criteria['startDate']);
+            $startDate->setTime(0, 0, 0);
+            $qb
+                ->andWhere('p.createdAt >= :startDate')
+                ->setParameter('startDate', $startDate);
+        }
+
+        if(!empty($criteria['endDate'])) {
+            $endDate = \DateTime::createFromFormat('d/m/Y', $criteria['endDate']);
+            $endDate->setTime(0, 0, 0);
+            $qb
+                ->andWhere('p.createdAt <= :endDate')
+                ->setParameter('endDate', $endDate);
+        }
+
+        return $qb;
+    }
 
 }
--- module/Application/src/Application/Repository/Proposal/WorkshopProposal.php
+++ module/Application/src/Application/Repository/Proposal/WorkshopProposal.php
@@ -14,4 +14,32 @@ use Util\Repository\AbstractRepository;
 class WorkshopProposal extends AbstractRepository
 {
 
+    public function prepareSearch($criteria = [], $orderBy = [])
+    {
+        $qb = $this->createQueryBuilder('p');
+
+        if(!empty($criteria['name'])) {
+            $qb
+                ->andWhere('p.name like :name')
+                ->setParameter('name', '%'.$criteria['name'].'%');
+        }
+
+        if(!empty($criteria['startDate'])) {
+            $startDate = \DateTime::createFromFormat('d/m/Y', $criteria['startDate']);
+            $startDate->setTime(0, 0, 0);
+            $qb
+                ->andWhere('p.createdAt >= :startDate')
+                ->setParameter('startDate', $startDate);
+        }
+
+        if(!empty($criteria['endDate'])) {
+            $endDate = \DateTime::createFromFormat('d/m/Y', $criteria['endDate']);
+            $endDate->setTime(0, 0, 0);
+            $qb
+                ->andWhere('p.createdAt <= :endDate')
+                ->setParameter('endDate', $endDate);
+        }
+
+        return $qb;
+    }
 }
