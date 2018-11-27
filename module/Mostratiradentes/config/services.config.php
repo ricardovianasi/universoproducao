bc3a98be6d9f38bdee69fba4a9ea1079940202e1
Ricardo Viana <ricardovianasi@gmail.com>
2018-11-27 15:04:54 -0200

    correcoes

--- module/Mostratiradentes/config/services.config.php
+++ module/Mostratiradentes/config/services.config.php
@@ -4,7 +4,7 @@ use Application\Navigation;
 return [
     'factories' => [
         'MostratiradentesNavigation' => function($e) {
-            $navigation = new Navigation\SiteNavigation(10, 'mostratiradentes/default');
+            $navigation = new Navigation\SiteNavigation(14, 'mostratiradentes/default');
             return $navigation->createService($e);
         },
     ],
