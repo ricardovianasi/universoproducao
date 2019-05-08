0bd2eddc25c434a224168b133d39749859749461
Ricardo Viana <ricardovianasi@gmail.com>
2019-05-08 15:06:04 -0300

    asdf

--- module/Cineop2019/src/Cineop2019/Controller/PostController.php
+++ module/Cineop2019/src/Cineop2019/Controller/PostController.php
@@ -136,7 +136,7 @@ class PostController extends SiteController
             $logistica		 				= $postData['logistica'];
 
             //Envia a mensagem de confirmação para a pessoa cadastrada no formulario
-            $msgConfirmacaoCadastro = "Informamos que sua solicitação de credenciamento de imprensa para a 13ª Mostra de Cinema de Ouro Preto foi enviada com sucesso.";
+            $msgConfirmacaoCadastro = "Informamos que sua solicitação de credenciamento de imprensa para a 14ª Mostra de Cinema de Ouro Preto foi enviada com sucesso.";
             $msgConfirmacaoCadastro .= "<br /><br />";
             $msgConfirmacaoCadastro .= "Em breve a equipe da assessoria entrará em contato.";
             $msgConfirmacaoCadastro .= "<br /><br />";
@@ -155,7 +155,7 @@ class PostController extends SiteController
             $mailConfirmacao->setBody($miniMessage);
             $mailConfirmacao->setFrom('no-reply@universoproducao.com.br', 'Universo Producao');
             $mailConfirmacao->setTo($email);
-            $mailConfirmacao->setSubject('Solicitação de credenciamento – 13a CineOP');
+            $mailConfirmacao->setSubject('Solicitação de credenciamento – 14a CineOP');
             $transport->send($mailConfirmacao);
 
             $msgFormulario  = "<h1>FORMUL&Aacute;RIO DE CREDENCIAMENTO</h1>";
