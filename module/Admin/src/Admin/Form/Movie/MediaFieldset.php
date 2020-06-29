bd292bcc0e13ddd4f550ab4c17259cced0d3649a
Ricardo Viana <ricardovianasi@gmail.com>
2020-06-29 15:15:16 -0300

    af

--- module/Admin/src/Admin/Form/Movie/MediaFieldset.php
+++ module/Admin/src/Admin/Form/Movie/MediaFieldset.php
@@ -27,7 +27,7 @@ class MediaFieldset extends Fieldset implements InputFilterProviderInterface
                 'label' => 'Créditos da foto'
             ],
             'attributes' => [
-                'placeholder' => 'Créditos da foto (caso não exista, favor incluir a palavra DIVULGAÇÃO)'
+                'placeholder' => 'Legenda (até 100 caracteres) e créditos da foto'
             ]
         ]);
 
