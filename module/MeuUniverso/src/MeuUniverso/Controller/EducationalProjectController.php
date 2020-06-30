0ffbcb1537954aa874cd7fd12f83bfa2397cb6f5
Ricardo Viana <ricardovianasi@gmail.com>
2020-06-30 19:34:07 -0300

    asdf

--- module/MeuUniverso/src/MeuUniverso/Controller/EducationalProjectController.php
+++ module/MeuUniverso/src/MeuUniverso/Controller/EducationalProjectController.php
@@ -87,7 +87,23 @@ class EducationalProjectController extends AbstractMeuUniversoRegisterController
                 $this->getRequest()->getFiles()->toArray()
             );
             $form->setData($data);
-            if($form->isValid()) {
+
+            $validFiles = false;
+            $files = $data['files'];
+            if($files) {
+                foreach ($files as $f) {
+                    if(empty(!$f['file']['name'])) {
+                        $validFiles = true;
+                        break;
+                    }
+                }
+            }
+
+            if(!$validFiles) {
+                $form->setMessages(['files'=>['erro'=>'Nenhuma imagem foi selecionada']]);
+            }
+
+            if($validFiles && $form->isValid()) {
 
                 if(!empty($data['category'])) {
                     $cat = $this->getRepository(Category::class)->find($data['category']);
