3bbd383a5091f63a765027f5a899fafb1c740c78
Ricardo Viana <ricardovianasi@gmail.com>
2020-07-14 14:47:05 -0300

    asdf

--- module/MeuUniverso/src/MeuUniverso/Controller/ProjectRegistrationController.php
+++ module/MeuUniverso/src/MeuUniverso/Controller/ProjectRegistrationController.php
@@ -225,7 +225,7 @@ class ProjectRegistrationController extends AbstractMeuUniversoRegisterControlle
                         $msg = '<p>Olá <strong>' . $user->getName() . '</strong>!</p>';
                         $msg .= '<p>Informamos que o projeto <strong>' . $project->getTitle() . '</strong> foi inscrito com sucesso 
                        para participar da seleção de projetos do <strong>11º Brasil CineMundi</strong>, a ser realizado 
-                       entre 22 a 27 de outubro de 2020 em ambiente online, como parte integrante da programação da 14ª Mostra CineBH.</p>';
+                       entre 29 de outubro a 02 de novembro de 2020 em ambiente online, como parte integrante da programação da 14ª Mostra CineBH.</p>';
                         $msg .= '<p>O resultado da seleção está previsto para ser divulgado em <strong>setembro de 2020</strong>, 
                         pelo site <a href="http://www.brasilcinemundi.com.br">www.brasilcinemundi.com.br</a>.</p>';
                         $msg .= '<p>Pedimos a gentileza de manter os dados do seu cadastro sempre atualizados para garantir 
